2d63d1b300064f5324b4f5825981ea3ebbd3a55e
SaiSushmita <71960577+SaiSushmita@users.noreply.github.com>
2020-10-07 23:54:14 +0530

    Add files via upload

--- Assign2_18_Balance.py
+++ Assign2_18_Balance.py
@@ -4,4 +4,19 @@ Created on Wed Oct  7 20:43:56 2020
 
 @author: Sushmita
 """
-
+balance = 0
+while(1):
+    s = input("Enter Deposit or Withdral data\n")
+    sarr = s.split(' ')
+    transac = sarr[0]
+    amount = int(sarr[1])
+    if transac == 'D':
+        balance = balance + amount
+    else:
+        balance = balance - amount
+    print("Balance is:",balance)
+    x = input("Do you want to continue? y/n\n")
+    if x == 'y' or x =='Y':
+        continue
+    else:
+        break
